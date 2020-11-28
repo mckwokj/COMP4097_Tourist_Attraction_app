fa5ac887baf771209e1d3a89167c4a289776c0c0
Johnson Kwok <mckwokj@gmail.com>
2020-11-28 16:26:20 +0800

    Developed map and navigation function

--- app/src/main/java/edu/hkbu/comp/comp4097/comp4097project/DetailFragment.kt
+++ app/src/main/java/edu/hkbu/comp/comp4097/comp4097project/DetailFragment.kt
@@ -1,21 +1,28 @@
 package edu.hkbu.comp.comp4097.comp4097project
 
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.squareup.picasso.Picasso
 import edu.hkbu.comp.comp4097.comp4097project.data.AppDatabase
-import edu.hkbu.comp.comp4097.comp4097project.data.Place
 import edu.hkbu.comp.comp4097.comp4097project.data.PlaceInfo
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlin.math.round
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -77,6 +84,58 @@ class DetailFragment : Fragment() {
             districtTextView.text = "District:${districtText}\nApproximate distance: " +
                     "${roundedDistance}m"
             descTextView.text = "Description:${description}"
+
+            val mapBtn: Button = view.findViewById(R.id.mapBtn)
+
+            mapBtn.setOnClickListener {
+
+                Log.d("coordinate", place?.lat.toString())
+                Log.d("coordinate", place?.lon.toString())
+
+                it.findNavController().navigate(R.id.action_detailFragment_to_mapsFragment2,
+                bundleOf(Pair("lat", place?.lat), Pair("lon", place?.lon), Pair("name", place?.name)))
+
+//                val builder = AlertDialog.Builder(context)
+//                builder.setTitle("Choose a navigation mode")
+//                    .setItems(
+//                        arrayOf("Driving", "Walking"),
+//                        DialogInterface.OnClickListener { dialog, which ->
+//                            // The 'which' argument contains the index position
+//                            // of the selected item
+//                            var gmmIntentUri: Uri? = null
+//
+//                            if (which == 0) {
+////                                Log.d("Mode", "Driving is clicked")
+//                                gmmIntentUri =
+//                                    Uri.parse("google.navigation:q=${place?.lat},${place?.lon}&mode=d")
+//
+//                            } else {
+////                                Log.d("Mode", "Walking is clicked")
+//                                gmmIntentUri =
+//                                    Uri.parse("google.navigation:q=${place?.lat},${place?.lon}&mode=w")
+//                            }
+//
+//                            // Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
+//                            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+//
+//                            // Make the Intent explicit by setting the Google Maps package
+//                            mapIntent.setPackage("com.google.android.apps.maps")
+//
+//                            val packageManager = requireActivity().packageManager
+//
+//                            // verify that at least one app that can handle the intent
+//                            mapIntent.resolveActivity(packageManager)?.let {
+//                                // Attempt to start an activity that can handle the Intent
+//                                startActivity(mapIntent)
+//                            }
+//                        })
+//                    .setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, which ->
+//                        Log.d("Mode", "Cancel is clicked")
+//                        dialog.cancel()
+//                    })
+//                builder.show()
+            }
+
 
 //            if (image_URL != null) {
 //                Log.d("DetailFragment", image_URL)
---
+++ app/src/main/java/edu/hkbu/comp/comp4097/comp4097project/MapsFragment.kt
@@ -0,0 +1,105 @@
+package edu.hkbu.comp.comp4097.comp4097project
+
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
+import android.net.Uri
+import androidx.fragment.app.Fragment
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapsFragment : Fragment() {
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+        val lat = arguments?.getDouble("lat")
+        val lon = arguments?.getDouble("lon")
+        val name = arguments?.getString("name")
+
+        if (lat != null && lon != null && name != null) {
+            val place = LatLng(lat, lon)
+            googleMap.addMarker(MarkerOptions().position(place).title(name))
+            googleMap.setMinZoomPreference(15F)
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(place))
+        }
+
+        val navBtn: Button = requireView().findViewById(R.id.navBtn)
+
+        navBtn.setOnClickListener {
+            val builder = AlertDialog.Builder(context)
+            builder.setTitle("Choose a navigation mode")
+                .setItems(
+                    arrayOf("Driving", "Walking"),
+                    DialogInterface.OnClickListener { dialog, which ->
+                        // The 'which' argument contains the index position
+                        // of the selected item
+                        var gmmIntentUri: Uri? = null
+
+                        if (which == 0) {
+//                                Log.d("Mode", "Driving is clicked")
+                            gmmIntentUri =
+                                Uri.parse("google.navigation:q=${lat},${lon}&mode=d")
+
+                        } else {
+//                                Log.d("Mode", "Walking is clicked")
+                            gmmIntentUri =
+                                Uri.parse("google.navigation:q=${lat},${lon}&mode=w")
+                        }
+
+                        // Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
+                        val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+
+                        // Make the Intent explicit by setting the Google Maps package
+                        mapIntent.setPackage("com.google.android.apps.maps")
+
+                        val packageManager = requireActivity().packageManager
+
+                        // verify that at least one app that can handle the intent
+                        mapIntent.resolveActivity(packageManager)?.let {
+                            // Attempt to start an activity that can handle the Intent
+                            startActivity(mapIntent)
+                        }
+                    })
+                .setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, which ->
+                    Log.d("Mode", "Cancel is clicked")
+                    dialog.cancel()
+                })
+            builder.show()
+        }
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_maps, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+    }
+}
